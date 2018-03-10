5c7db253f2cb2c6905652f1b936cc2c55701b2e5
Valerii <vkuzin@zaraffasoft.com>
2018-03-10 21:28:32 +0200

    #1.15 Instaled youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 
+import SearchBar from './components/search_bar'
+
+const API_KEY = 'AIzaSyBzvDI8wjev18vnHkSEriX1y7gIqzX6VzA';
+
+
 const App = () => {
-    return <div>Hi!!!</div>;
-}
+    return (
+        <div>
+            <SearchBar  />
+        </div>
+    );
+};
 
 ReactDom.render(<App />, document.querySelector('.container'));
