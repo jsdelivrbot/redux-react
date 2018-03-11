ef037469b02dcaf6455c83b497491d9de695b9dc
Valerii <vkuzin@zaraffasoft.com>
2018-03-11 02:09:15 +0200

    #2.22 Made class Add

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import ReactDom from 'react-dom'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
 
 const API_KEY = 'AIzaSyBzvDI8wjev18vnHkSEriX1y7gIqzX6VzA';
 
 
-const App = () => {
-    return (
-        <div>
-            <SearchBar  />
-        </div>
-    );
-};
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'react redux'}, function (videos) {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar  />
+            </div>
+        );
+    }
+}
 
 ReactDom.render(<App />, document.querySelector('.container'));
