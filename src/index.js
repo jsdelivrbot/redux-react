1f23d2432f1f488aff2e970c9fce3b2c8decea38
Valerii <vkuzin@zaraffasoft.com>
2018-03-11 13:42:08 +0200

    #2.23 Added props for send search videos

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search_bar'
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyBzvDI8wjev18vnHkSEriX1y7gIqzX6VzA';
 
@@ -13,7 +14,7 @@ class App extends Component {
 
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'react redux'}, function (videos) {
+        YTSearch({key: API_KEY, term: 'react redux'}, (videos) => {
             this.setState({ videos });
         });
     }
@@ -21,7 +22,8 @@ class App extends Component {
     render() {
         return (
             <div>
-                <SearchBar  />
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
             </div>
         );
     }
